838c9fc113610afe84dbec36c992b308682f22bc
Birukou Aliaksei <Birukou.aliaksei@gmail.com>
2021-04-08 21:05:42 +0300

    fixed Exercise: Loops and Functions

--- basic/exercise-loops-and-functions.go
+++ basic/exercise-loops-and-functions.go
@@ -6,13 +6,11 @@ import (
 )
 
 func Sqrt(x float64) float64 {
-	z := 1.0
-	for i := 0; i < 10; i++ {
-		fmt.Println(math.Sqrt(x))
-	}
-	return z
+	return math.Sqrt(x)
 }
 
 func main() {
-	fmt.Println(Sqrt(5))
+	for i := 0; i < 10; i++ {
+		fmt.Println(Sqrt(2))
+	}
 }
